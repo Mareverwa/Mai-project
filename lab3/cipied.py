c7452f1405de428db729ec14dd3f7073302bfd0f
Mareverwa <113584021+Mareverwa@users.noreply.github.com>
2022-11-17 02:35:18 +0300

    lab4

---
+++ lab3/cipied.py
@@ -0,0 +1,4 @@
+numbers =int(input("enter the numbers")) 
+count =0
+count = count +numbers.count(0)
+print('count of 0 :',count)
---
+++ lab4/assignment4.py
@@ -0,0 +1,6 @@
+def get(d, k):
+    print(d.get(k))
+
+# пример    
+
+get({"name": "Talent",'surname' :'Mareverwa'}, "surname" )
